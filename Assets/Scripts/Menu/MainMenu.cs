9dc5ce7a490b9d3c7c176fc6a377f5628982eb31
Fujii <rafael_fujii88@hotmail.com>
2017-12-02 17:47:33 -0200

    Menus

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
