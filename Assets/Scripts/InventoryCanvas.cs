92efa630056910dbbe1cc0c973eaf08c2f1d7603
Mateus Ribeiro Culpo <mateusculpo@gmail.com>
2017-12-02 22:20:38 -0200

    Commit

---
+++ Assets/Scripts/InventoryCanvas.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryCanvas : MonoBehaviour
+{
+    private PlayerInventory m_PlayerInventory;
+    [SerializeField]
+    private Text m_TextBiscuit;
+    [SerializeField]
+    private Text m_TextShit;
+
+    private void Awake()
+    {
+        m_PlayerInventory = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>();
+    }
+
+    public void UpdateShit()
+    {
+        //m_TextShit.text = m_PlayerInventory
+    }
+    public void UpdateBiscuit() { }
+    public void UpdateToxic() { }
+}
